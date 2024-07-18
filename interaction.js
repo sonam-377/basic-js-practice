60c52cd4df6841a42394316702397bd4f4774ac7
sonam_00 <sonam9tamang45@gmail.com>
2024-07-18 17:55:49 +0545

    learned about interaction

---
+++ interaction.js
@@ -0,0 +1,10 @@
+//alert("hello world");
+
+//prompt
+const age = prompt("how old are you?");
+
+//confirm
+const abbove18 = confirm("are you above 18 years old?");
+console.log(age, above18);
+
+alert(`you are ${age} years old`); // string literal
