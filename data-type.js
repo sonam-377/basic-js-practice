134cc76382acaaa942f6713a378df65fa2724e05
sonam_00 <sonam9tamang45@gmail.com>
2024-07-19 17:07:00 +0545

    data types

---
+++ data-type.js
@@ -0,0 +1,20 @@
+//data type
+const a = "2";
+console.log(a);
+
+//type conversion
+const b = Number("a");
+
+const num1 = prompt("enter 1st number");
+const num2 = prompt("enetr second number");
+const sum = Number(num1) + Number(num2);
+alert(`sum of numbers is ${sum}`);
+
+//data types 5 types
+//1. primitive datatypes
+// 2.complex datatypes
+// type coversion/coversion
+// const a="2";
+// console.log(typeof a); // check type
+// const num = Number(a);//type converison
+// console.log(num);
