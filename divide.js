27bf4bdc5d2930c34aec11089058e6b413f0bd4e
sonam_00 <sonam9tamang45@gmail.com>
2024-07-19 15:59:09 +0545

    divide completd

---
+++ divide.js
@@ -0,0 +1,9 @@
+//write a js code to divide in calculator
+
+alert("welcome to sonam's calculator");
+const pehlaNumber = parseFloat(prompt("enter your first number"));
+const dusraNumber = parseFloat(prompt("enter your second number"));
+const div = pehlaNumber / dusraNumber;
+console.log(div);
+const sure = confirm("do u want to divide two numbers");
+alert(`your multipled numeber is ${div}`);
