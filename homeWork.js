1b0a31925fb9ebc5fe57d63817b6780ba8e71e6c
sonam_00 <sonam9tamang45@gmail.com>
2024-07-19 12:47:16 +0545

    completed homework

---
+++ homeWork.js
@@ -0,0 +1,16 @@
+//write a js program to create sum calcualtor
+
+alert("calculator ma swagat xa hajurlai");
+
+//prompt
+const pahiloNumber = parseInt(prompt("enter first number"));
+const dosroNumber = parseInt(prompt("enter second number"));
+
+//confirm
+const sure = confirm("do u want to add two numbers?");
+//adding
+const sum = pahiloNumber + dosroNumber;
+
+console.log(sum);
+
+alert(`your sum of numbers is ${sum}`);
