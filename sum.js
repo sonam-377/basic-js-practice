2c174ba97960177b1511215f0dafbc0151813002
sonam_00 <sonam9tamang45@gmail.com>
2024-07-20 12:07:54 +0545

    sum pani practice garey

---
+++ sum.js
@@ -0,0 +1,5 @@
+const num1 = Number(prompt("enter your 1st number"));
+const num2 = Number(prompt("enter your 2nd number"));
+const sum = num1 + num2;
+const ask = confirm("are u sure you want to add these two numbers?");
+alert(`the sum of two numbers are ${sum}`);
