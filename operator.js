f40ac6ca37b74a94172f6a1ea631c34b9572039d
sonam_00 <sonam9tamang45@gmail.com>
2024-07-20 11:55:28 +0545

    operators learnt

---
+++ operator.js
@@ -0,0 +1,33 @@
+// operatorss
+// AND
+// OR
+// NOT
+
+// AND (both conditioon must be true)
+// coditon1 && condition2
+// true && true
+
+//if (condition1 && condition 2){
+// logic 1
+//} else {
+//logic 2
+//}
+
+// 2. OR (any 1 condtion must be true)
+// conditon1 || condtion2
+
+// 3. NOT (opposite value)
+
+// const isAdult = true;
+// if (!isAdult) {
+//   console.log("you are watching cartoon");
+// } else {
+//   console.log("your are watching news");
+// }
+const ask = prompt("are you adult or child?");
+const adult = true;
+if (!adult) {
+  console.log("u are watching funny cartoons");
+} else {
+  console.log("you are watching horror");
+}
