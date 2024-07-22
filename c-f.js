e8aff8654facc33f19b4d06865accab354f23afc
sonam_00 <sonam9tamang45@gmail.com>
2024-07-22 15:31:15 +0545

    celcius to fahrenheit donee

---
+++ c-f.js
@@ -0,0 +1,10 @@
+// Celsius temperature
+let celsiusTemperature = 60;
+
+// Conversion formula: (C × 9/5) + 32
+let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+
+// Output the result
+console.log(
+  `${celsiusTemperature} degrees Celsius is equal to ${fahrenheitTemperature} degrees Fahrenheit.`
+);
